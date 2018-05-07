01aab05617016eb69c84ac4cd662bba2101f6177
Gabriel West <gabe_west1@yahoo.com>
2018-05-07 14:52:30 -0500

    Refactored Todo to be a functional component and its state now resides in the App.js.
    Added form to create new todos.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react'
 import styled from "styled-components"
+
 import Todo from "./Todo.js"
+import TodoModel from "./TodoModel"
 
 class App extends Component {
+  state = {
+    todos: [
+      new TodoModel("Delete this")
+    ]
+  }
   render() {
+    const {todos} = this.state
     return (
       <AppView>
-        <Todo />
+        <TodoForm>
+          <input type="text" name="new-todo" />
+          <AddTodo onClick={this._createTodo}>Add Todo</AddTodo>
+        </TodoForm>
+        <Todo todos={todos} />
       </AppView>
     )
   }
+  _createTodo = (e) => {
+    e.preventDefault()
+    const todoText = document.querySelector("[name=new-todo]").value
+    const newTodo = new TodoModel(todoText)
+    
+    this.setState({todos: this.state.todos.concat(newTodo)})
+  }
 }
 
+const AddTodo = styled.button``
+const TodoForm = styled.form``
 const AppView = styled.div`
   height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `
--- src/Todo.js
+++ src/Todo.js
@@ -1,29 +1,18 @@
 import React from "react"
 import styled from "styled-components"
 
-import TodoModel from "./TodoModel"
-
-export default class Todo extends React.Component{
-    state = { todos: [
-        new TodoModel("Delete this")
-    ] }
-    render() {
-        const {todos} = this.state
-
-        return (
-            <TodoView {...this.props}>
-                { todos.map((todo) => 
-                    <TodoItem 
-                        key={todo.getText()}
-                        color={todo.getColor()}
-                    >
-                        {todo.getText()}
-                    </TodoItem>
-                )}
-            </TodoView>
-        )
-    }
-} 
+export default props => (
+    <TodoView {...props}>
+        { props.todos.map((todo) => 
+            <TodoItem 
+                key={todo.getText()}
+                color={todo.getColor()}
+            >
+                {todo.getText()}
+            </TodoItem>
+        )}
+    </TodoView>
+)
 
 const TodoItem = styled.li`
     height: 120px;
