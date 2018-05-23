265e2f13973aa7caa86582401c9c8cafde68ac09
Gabriel West <gabe_west1@yahoo.com>
2018-05-23 13:30:25 -0500

    Added empty tests to add/delete/modify a todo

---
+++ cypress/integration/App_spec.js
@@ -0,0 +1,15 @@
+function getElement(selector) {
+    return cy.get(`[data-test=${selector}`)
+}
+
+describe("Todo Application", () => {
+    it("Adds a new todo", () => {
+        
+    })
+    it("Deletes a todo", () => {
+
+    })
+    it("Modifies a todo", () => {
+
+    })
+}) 
