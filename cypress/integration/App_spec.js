d3146896786d9d28d96382ae3a2ba1b991dbaa62
Gabriel West <gabe_west1@yahoo.com>
2018-05-23 13:46:23 -0500

    Updated add new todo test to be in a failing state.

--- cypress/integration/App_spec.js
+++ cypress/integration/App_spec.js
@@ -3,8 +3,18 @@ function getElement(selector) {
 }
 
 describe("Todo Application", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000")
+    })
     it("Adds a new todo", () => {
-        
+        const numTodosBefore = getElement("todos").children().length
+
+        getElement("newTodoInput").type("This is a new todo")
+        getElement("newTodoBtn").click()
+
+        const numTodosAfter = getElement("todos").children().length
+
+        expect(numTodosBefore + 1).to.equal(numTodosAfter)
     })
     it("Deletes a todo", () => {
 
