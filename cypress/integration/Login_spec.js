3207615f4f69415fa0e277aa37c15947581239db
Gabriel West <gabe_west1@yahoo.com>
2018-06-15 00:52:31 -0500

    Wrote tests to log in a user by their email

--- cypress/integration/Login_spec.js
+++ cypress/integration/Login_spec.js
@@ -7,8 +7,15 @@ describe("Login and Registering", () => {
   })
 
   it("should log the user in by email", () => {
+    const expectedEndingURL = "http://localhost:3000/home"
+
     getElement("loginUsername").type("test-email@yahoo.com")
     getElement("loginPassword").type("test-password")
     getElement("loginSubmit").click()
+
+    cy.wait(2000)
+    cy.url().then(actualEndingURL => {
+      expect(actualEndingURL).to.equal(expectedEndingURL)
+    })
   })
 })
