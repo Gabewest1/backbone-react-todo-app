f0cc4f5ed077d564266b80e79c0de4b85ce8d4ac
Gabriel West <gabe_west1@yahoo.com>
2018-06-15 01:03:54 -0500

    Added comment explaining a possible failure for the test: 'should log the user in by email'

--- cypress/integration/Login_spec.js
+++ cypress/integration/Login_spec.js
@@ -13,6 +13,9 @@ describe("Login and Registering", () => {
     getElement("loginPassword").type("test-password")
     getElement("loginSubmit").click()
 
+    //This 2s wait is based on the fact that logging in takes a
+    //minimum of 2s. This isn't the best solution b/c if logging in
+    //takes more than 2s then we fail D:
     cy.wait(2000)
     cy.url().then(actualEndingURL => {
       expect(actualEndingURL).to.equal(expectedEndingURL)
