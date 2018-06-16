51a0d01487776dde5bc25f8ae98154a886fdfcce
Gabriel West <gabe_west1@yahoo.com>
2018-06-15 19:37:27 -0500

    Wrote passing test for failing to log in a user with an incorrect username.

--- cypress/integration/Login_spec.js
+++ cypress/integration/Login_spec.js
@@ -21,4 +21,32 @@ describe("Login and Registering", () => {
       expect(actualEndingURL).to.equal(expectedEndingURL)
     })
   })
+  it("should log the user in by username", () => {
+    const expectedEndingURL = "http://localhost:3000/home"
+
+    getElement("loginUsername").type("test")
+    getElement("loginPassword").type("test-password")
+    getElement("loginSubmit").click()
+
+    //This 2s wait is based on the fact that logging in takes a
+    //minimum of 2s. This isn't the best solution b/c if logging in
+    //takes more than 2s then we fail D:
+    cy.wait(2000)
+    cy.url().then(actualEndingURL => {
+      expect(actualEndingURL).to.equal(expectedEndingURL)
+    })
+  })
+  it("should fail to log the user in with an incorrect username", () => {
+    const expectedEndingURL = "http://localhost:3000/home"
+
+    getElement("loginUsername").type("non-real-username")
+    getElement("loginPassword").type("test-password")
+    getElement("loginSubmit").click()
+
+    //This 2s wait is based on the fact that logging in takes a
+    //minimum of 2s. This isn't the best solution b/c if logging in
+    //takes more than 2s then we fail D:
+    cy.wait(2000)
+    getElement("loginUsernameError").then(el => expect(el).to.exist)
+  })
 })
