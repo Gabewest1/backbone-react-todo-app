604ad577d989a6100d05f7d9856632ea812f307c
Gabriel West <gabe_west1@yahoo.com>
2018-06-16 00:29:21 -0500

    Wrote test: 1) should fail to log the user in with a correct email but incorrect password 2 ) should fail to log the user in with a correct username but incorrect password 3) should fail to log the user in with no password & username value

--- cypress/integration/Login_spec.js
+++ cypress/integration/Login_spec.js
@@ -5,39 +5,39 @@ function waitForLogin() {
   //This 2s wait is based on the fact that logging in takes a
   //minimum of 2s. This isn't the best solution b/c if logging in
   //takes more than 2s then we fail D:
-  cy.wait(2300)
+  cy.wait(2100)
 }
 describe("Login and Registering", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000")
   })
 
-  // it("should log the user in by email", () => {
-  //   const expectedEndingURL = "http://localhost:3000/home"
+  it("should log the user in by email", () => {
+    const expectedEndingURL = "http://localhost:3000/home"
 
-  //   getElement("loginUsername").type("test-email@yahoo.com")
-  //   getElement("loginPassword").type("test-password")
-  //   getElement("loginSubmit").click()
+    getElement("loginUsername").type("test-email@yahoo.com")
+    getElement("loginPassword").type("test-password")
+    getElement("loginSubmit").click()
 
-  //   waitForLogin()
+    waitForLogin()
 
-  //   cy.url().then(actualEndingURL => {
-  //     expect(actualEndingURL).to.equal(expectedEndingURL)
-  //   })
-  // })
-  // it("should log the user in by username", () => {
-  //   const expectedEndingURL = "http://localhost:3000/home"
+    cy.url().then(actualEndingURL => {
+      expect(actualEndingURL).to.equal(expectedEndingURL)
+    })
+  })
+  it("should log the user in by username", () => {
+    const expectedEndingURL = "http://localhost:3000/home"
 
-  //   getElement("loginUsername").type("test")
-  //   getElement("loginPassword").type("test-password")
-  //   getElement("loginSubmit").click()
+    getElement("loginUsername").type("test")
+    getElement("loginPassword").type("test-password")
+    getElement("loginSubmit").click()
 
-  //   waitForLogin()
+    waitForLogin()
 
-  //   cy.url().then(actualEndingURL => {
-  //     expect(actualEndingURL).to.equal(expectedEndingURL)
-  //   })
-  // })
+    cy.url().then(actualEndingURL => {
+      expect(actualEndingURL).to.equal(expectedEndingURL)
+    })
+  })
   it("should fail to log the user in with an incorrect username", () => {
     const expectedEndingURL = "http://localhost:3000/home"
 
@@ -49,6 +49,30 @@ describe("Login and Registering", () => {
 
     getElement("loginUsernameError").should("exist")
     getElement("loginPasswordError").should("not.exist")
+  })
+  it("should fail to log the user in with a correct email but incorrect password", () => {
+    const expectedEndingURL = "http://localhost:3000/home"
+
+    getElement("loginUsername").type("test-email@yahoo.com")
+    getElement("loginPassword").type("bad password")
+    getElement("loginSubmit").click()
+
+    waitForLogin()
+
+    getElement("loginUsernameError").should("not.exist")
+    getElement("loginPasswordError").should("exist")
+  })
+  it("should fail to log the user in with a correct username but incorrect password", () => {
+    const expectedEndingURL = "http://localhost:3000/home"
+
+    getElement("loginUsername").type("test")
+    getElement("loginPassword").type("bad password")
+    getElement("loginSubmit").click()
+
+    waitForLogin()
+
+    getElement("loginUsernameError").should("not.exist")
+    getElement("loginPasswordError").should("exist")
   })
   it("should fail to log the user in with an no username value", () => {
     const expectedEndingURL = "http://localhost:3000/home"
@@ -71,5 +95,15 @@ describe("Login and Registering", () => {
 
     getElement("loginPasswordError").should("exist")
     getElement("loginUsernameError").should("not.exist")
+  })
+  it("should fail to log the user in with no password & username value", () => {
+    const expectedEndingURL = "http://localhost:3000/home"
+
+    getElement("loginSubmit").click()
+
+    waitForLogin()
+
+    getElement("loginPasswordError").should("exist")
+    getElement("loginUsernameError").should("exist")
   })
 })
