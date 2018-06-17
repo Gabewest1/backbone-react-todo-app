857d4f59495b47c941b6b3689eda6fbc7b79307f
Gabriel West <gabe_west1@yahoo.com>
2018-06-16 23:53:56 -0500

    Moved code related to setting up passport from server.js into passportSetup.js

---
+++ backend/passportSetup.js
@@ -0,0 +1,104 @@
+const passport = require("passport")
+const UserModel = require("./UserModel")
+const LocalStrategy = require("passport-local").Strategy
+
+passport.use(
+  "local-login",
+  new LocalStrategy((username, password, done) => {
+    console.log("LOGGING in:", username, password)
+
+    const query = {
+      $or: [
+        { username: { $regex: `^${username}$`, $options: "i" } },
+        { email: { $regex: `^${username}$`, $options: "i" } },
+      ],
+    }
+
+    UserModel.findOne(query, (err, user) => {
+      if (err) {
+        console.log(err)
+        done(err)
+      } else {
+        const errors = {}
+
+        const foundUser = user && (user.username === username || user.email === username)
+        const isValidPassword = foundUser && user.validPassword(password)
+
+        errors.username = !foundUser ? "User not found" : undefined
+
+        if (!errors.username) {
+          errors.password = !isValidPassword ? "Incorrect password" : undefined
+        }
+
+        const isTheirErrors = errors.username || errors.password
+
+        if (isTheirErrors) {
+          done(null, false, errors)
+        } else {
+          console.log("FOUND USER:", user)
+          done(null, user)
+        }
+      }
+    })
+  })
+)
+
+passport.use(
+  "local-signup",
+  new LocalStrategy({ passReqToCallback: true }, (req, username, password, next) => {
+    username = req.body.username.toLowerCase()
+    password = req.body.password.toLowerCase()
+    email = req.body.email.toLowerCase()
+
+    console.log("SIGNING UP:", email, username, password)
+
+    const query = {
+      $or: [
+        { username: { $regex: `^${username}$`, $options: "i" } },
+        { email: { $regex: `^${email}$`, $options: "i" } },
+      ],
+    }
+
+    UserModel.findOne(query, (err, user) => {
+      if (err) {
+        console.log(err)
+        res.sendStatus(500)
+      } else if (user) {
+        const errors = {}
+
+        errors.username = user.username === username ? "Username taken" : undefined
+        errors.email = user.email === email ? "Email taken" : undefined
+
+        console.log("FOUND AN EXISTING USER:", errors)
+        next(null, null, errors)
+      } else {
+        console.log("ABOUT TO MAKE NEW USER")
+        const newUser = new UserModel()
+
+        newUser.username = username
+        newUser.email = email
+        newUser.password = newUser.generateHash(password)
+
+        newUser.save((err, user) => {
+          if (err) {
+            console.log(err)
+            next(err)
+          } else {
+            console.log("Created new User:", user)
+            next(null, user)
+          }
+        })
+      }
+    })
+  })
+)
+
+passport.serializeUser((user, done) => {
+  console.log(`serializeUser: ${user}`)
+  done(null, user)
+})
+
+passport.deserializeUser((user, done) => {
+  console.log(`deserializeUser: ${user}`)
+  done(null, user)
+})
--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,14 @@ const cookieSession = require("cookie-session")
 const cookieParser = require("cookie-parser")
 const path = require("path")
 const passport = require("passport")
-const LocalStrategy = require("passport-local").Strategy
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
 const UserModel = require("./UserModel")
+
+//Setup Passport
+require("./passportSetup")
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -44,8 +46,9 @@ app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "build", "index.html"))
 })
 
-app.put("/saveme", (req, res) => {
-  console.log("SAVEME")
+app.post("/saveTodos", (req, res) => {
+  const { todos } = req.body
+  console.log("Saving todos:", todos)
   res.end()
 })
 
@@ -108,107 +111,6 @@ app.post(
     res.json(signupErrors)
   }
 )
-
-passport.use(
-  "local-login",
-  new LocalStrategy((username, password, done) => {
-    console.log("LOGGING in:", username, password)
-
-    const query = {
-      $or: [
-        { username: { $regex: `^${username}$`, $options: "i" } },
-        { email: { $regex: `^${username}$`, $options: "i" } },
-      ],
-    }
-
-    UserModel.findOne(query, (err, user) => {
-      if (err) {
-        console.log(err)
-        done(err)
-      } else {
-        const errors = {}
-
-        const foundUser = user && (user.username === username || user.email === username)
-        const isValidPassword = foundUser && user.validPassword(password)
-
-        errors.username = !foundUser ? "User not found" : undefined
-
-        if (!errors.username) {
-          errors.password = !isValidPassword ? "Incorrect password" : undefined
-        }
-
-        const isTheirErrors = errors.username || errors.password
-
-        if (isTheirErrors) {
-          done(null, false, errors)
-        } else {
-          console.log("FOUND USER:", user)
-          done(null, user)
-        }
-      }
-    })
-  })
-)
-
-passport.use(
-  "local-signup",
-  new LocalStrategy({ passReqToCallback: true }, (req, username, password, next) => {
-    username = req.body.username.toLowerCase()
-    password = req.body.password.toLowerCase()
-    email = req.body.email.toLowerCase()
-
-    console.log("SIGNING UP:", email, username, password)
-
-    const query = {
-      $or: [
-        { username: { $regex: `^${username}$`, $options: "i" } },
-        { email: { $regex: `^${email}$`, $options: "i" } },
-      ],
-    }
-
-    UserModel.findOne(query, (err, user) => {
-      if (err) {
-        console.log(err)
-        res.sendStatus(500)
-      } else if (user) {
-        const errors = {}
-
-        errors.username = user.username === username ? "Username taken" : undefined
-        errors.email = user.email === email ? "Email taken" : undefined
-
-        console.log("FOUND AN EXISTING USER:", errors)
-        next(null, null, errors)
-      } else {
-        console.log("ABOUT TO MAKE NEW USER")
-        const newUser = new UserModel()
-
-        newUser.username = username
-        newUser.email = email
-        newUser.password = newUser.generateHash(password)
-
-        newUser.save((err, user) => {
-          if (err) {
-            console.log(err)
-            next(err)
-          } else {
-            console.log("Created new User:", user)
-            next(null, user)
-          }
-        })
-      }
-    })
-  })
-)
-
-passport.serializeUser((user, done) => {
-  console.log(`serializeUser: ${user}`)
-  done(null, user)
-})
-
-passport.deserializeUser((user, done) => {
-  console.log(`deserializeUser: ${user}`)
-  done(null, user)
-})
 
 //Connect to database
 mongoose.connect(
